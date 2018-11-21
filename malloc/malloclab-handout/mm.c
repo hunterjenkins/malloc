435d51079a0ce0b1d0aeab24f542869955a0e4da
Hunter Jenkins <hunterbjenkins@gmail.com>
2018-11-21 16:38:30 -0700

    d

--- malloc/malloclab-handout/mm.c
+++ malloc/malloclab-handout/mm.c
@@ -20,20 +20,10 @@
 #define FTRP(bp) ((char *)(bp)+GET_SIZE(HDRP(bp))-OVERHEAD)
 
 
-//returns the size of the given block
-// #define GET_SIZE(p)  ((block_header *)(p))->size
-
-//returns whether the block is allocated or not
-// #define GET_ALLOC(p) ((block_header *)(p))->allocated
-
 //returns a payload pointer for the next block, given a payload pointer
 #define NEXT_BLKP(bp) ((char *)(bp) + GET_SIZE(HDRP(bp)))
 #define PREV_BLKP(bp) ((char *)(bp)-GET_SIZE((char *)(bp)-OVERHEAD))
 
-
-
-
-//TODO: how to use these new macros?
 #define GET(p) (*(size_t *)(p))
 
 
@@ -57,15 +47,8 @@ void *first_bp;
 void mm_init(void *heap, size_t heap_size)
 {
   void *bp;
-  // void *bp = heap + sizeof(header) + 8;
-  // bp = heap + sizeof(block_header) + 8;
-  printf("Heap size: %ld\n", heap_size);
-
-  // here's a place where we depend on
-  // block_header being a multiple of ALIGNMENT:
   bp = heap + sizeof(block_header) + 8;
 
-  printf("A\n");
   // relying on heap_size being a multiple of 16,
   // which is part of the spec for mm_init
   //assuming block header is a multiple of alignment
@@ -73,14 +56,11 @@ void mm_init(void *heap, size_t heap_size)
   //this is actually setting the size
   PUT(HDRP(bp), PACK((heap_size - ( sizeof(block_header) + sizeof(block_footer))), 0 ));
 
-
   first_bp = bp;
 
   //create terminator block
   PUT(HDRP(NEXT_BLKP(bp)), PACK(0, 1));
-
   mm_malloc(0);
-
   return 0;
 }
 
@@ -111,7 +91,6 @@ static void set_allocated(void *bp, size_t size)
 //Size = how many bytes we want to allocate
 void *mm_malloc(size_t size)
 {
-  printf("mm_malloc\n");
   void *bp = first_bp; //I don't know if we'll want this to be the first_bp. TODO
 
   //Don't stop until we've found a place or every place is taken up
@@ -121,7 +100,6 @@ void *mm_malloc(size_t size)
     //Find the null terminator block
     if (GET_SIZE(HDRP(bp)) == 0)
     {
-      printf("Found the null terminator block\n");
       return NULL;
       break;
     }
@@ -138,8 +116,6 @@ void *mm_malloc(size_t size)
       return bp;
     }
   }
-
-
 }
 
 
@@ -148,8 +124,6 @@ void *coalesce(void *bp);
 void mm_free(void *bp)
 {
   // Callers must ensure that bp is actually allocated
-  printf("mm_free\n");
-
   size_t sz = GET_SIZE(HDRP(bp));
   PUT(HDRP(bp), PACK(sz, 0));
 
@@ -158,7 +132,6 @@ void mm_free(void *bp)
 
 
 void *coalesce(void *bp) {
-  printf("coalesce\n");
 
   size_t prev_alloc = GET_ALLOC(HDRP(PREV_BLKP(bp)));
   size_t next_alloc = GET_ALLOC(HDRP(NEXT_BLKP(bp)));
@@ -166,7 +139,6 @@ void *coalesce(void *bp) {
 
 
   if (prev_alloc && next_alloc) { /* Case 1 */
-    /* nothing to do */
   }
 
   else if (prev_alloc && !next_alloc) { /* Case 2 */
@@ -177,10 +149,8 @@ void *coalesce(void *bp) {
 
   else if (!prev_alloc && next_alloc) { /* Case 3 */
      size += GET_SIZE(HDRP(PREV_BLKP(bp)));
-     // GET_SIZE(FTRPI(bp)) = size;
      PUT(FTRP(bp), PACK(size, 0));
      PUT(HDRP(PREV_BLKP(bp)), PACK(size, 0));
-     // GET_SIZE(HDRP(PREV_BLKP(bp))) = size;
      bp = PREV_BLKP(bp);
    }
 
@@ -188,10 +158,7 @@ void *coalesce(void *bp) {
      size += (GET_SIZE(HDRP(PREV_BLKP(bp))) + GET_SIZE(HDRP(NEXT_BLKP(bp))));
 
      PUT(HDRP(PREV_BLKP(bp)), PACK(size, 0));
-     // GET_SIZE(HDRP(PREV_BLKP(bp))) = size;
-
      PUT(FTRP(PREV_BLKP(bp)), PACK(size, 0));
-     // GET_SIZE(FTRP(NEXT_BLKP(bp))) = size;
      bp = PREV_BLKP(bp);
    }
 
